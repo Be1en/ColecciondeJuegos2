c3f7c48e6655d7482ea530b3fff08ae65fafc43f
Be1en <valeria.godoy@tecsup.edu.pe>
2024-05-09 01:09:18 -0500

    Initial Commit

---
+++ GodoyColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GodoyColeccionDeJuegos
+//
+//  Created by valeria godoy on 9/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GodoyColeccionDeJuegosUITests/GodoyColeccionDeJuegosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GodoyColeccionDeJuegosUITestsLaunchTests.swift
+//  GodoyColeccionDeJuegosUITests
+//
+//  Created by valeria godoy on 9/05/24.
+//
+
+import XCTest
+
+final class GodoyColeccionDeJuegosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
